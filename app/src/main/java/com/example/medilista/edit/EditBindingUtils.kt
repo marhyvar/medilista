a6f124908ada952a6788a34baf4fb09d74c78c7e
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2021-05-15 10:59:37 +0300

    Edit bindingAdapter text

--- app/src/main/java/com/example/medilista/edit/EditBindingUtils.kt
+++ app/src/main/java/com/example/medilista/edit/EditBindingUtils.kt
@@ -2,11 +2,12 @@ package com.example.medilista.edit
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.example.medilista.combineAmountAndTimes
 import com.example.medilista.database.Dosage
 
 @BindingAdapter("testi")
 fun TextView.setDosageTestText(item: Dosage?) {
     item?.let {
-        text = item.amount.toString()
+        text = combineAmountAndTimes(item.amount, item.timeValueHours, item.timeValueMinutes)
     }
 }
