1aabe8e079a6acc5bd74ee015533a4a116ff0da2
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2021-07-12 17:50:54 +0300

    Add contentDescription for edit and delete buttons and edit margin

--- app/src/main/java/com/example/medilista/edit/EditBindingUtils.kt
+++ app/src/main/java/com/example/medilista/edit/EditBindingUtils.kt
@@ -1,5 +1,6 @@
 package com.example.medilista.edit
 
+import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.medilista.combineAmountAndTimes
@@ -7,7 +8,7 @@ import com.example.medilista.combineNameAndStrength
 import com.example.medilista.database.Dosage
 import com.example.medilista.database.Medicine
 
-@BindingAdapter("testi")
+@BindingAdapter("dosageDescription")
 fun TextView.setDosageTestText(item: Dosage?) {
     item?.let {
         text = combineAmountAndTimes(item.amount, item.timeValueHours, item.timeValueMinutes)
@@ -18,5 +19,21 @@ fun TextView.setDosageTestText(item: Dosage?) {
 fun TextView.setNameText(item: Medicine?) {
     item?.let {
         text = combineNameAndStrength(item.medicineName, item.strength, item.form)
+    }
+}
+
+@BindingAdapter("editButtonContentDescription")
+fun Button.setContentDescriptionForEdit(item: Dosage?) {
+    item?.let {
+        val text = combineAmountAndTimes(item.amount, item.timeValueHours, item.timeValueMinutes)
+        contentDescription = "Muokkaa annostusta $text"
+    }
+}
+
+@BindingAdapter("deleteButtonContentDescription")
+fun Button.setContentDescriptionForDelete(item: Dosage?) {
+    item?.let {
+        val text = combineAmountAndTimes(item.amount, item.timeValueHours, item.timeValueMinutes)
+        contentDescription = "Poista annostus $text"
     }
 }
